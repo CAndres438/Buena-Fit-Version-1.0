65c41a8a75ae977f65874b5f733bafe2517c60c5
CAndres438 <candresortiz123@gmail.com>
2022-05-09 19:01:14 -0500

    arreglo de error con git

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,10 +31,7 @@ const Home = () => {
                 <span className={classes.category__workoutSpan}>{image.categoria}</span>
                 </div>
               </div>
-<<<<<<< HEAD
 
-=======
->>>>>>> AndresDev2
               <div className={classes.category__workoutFilter}>
                 
                 <div className={classes.category__workoutPathBox} onMouseEnter={() => setImage({
